5eb8a2c8959c606789420e909fe913a07d7f5af8
yash.patel <patelyash045@gmail.com>
2021-11-25 18:00:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mytryapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.mytryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
